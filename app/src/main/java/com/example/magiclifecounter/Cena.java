761c2721de55d119f47fd7908b21d7d45f4c55d0
augusto pugas <augustopugas34@gmail.com>
2021-12-01 19:02:21 -0300

    Adicionando o placar para o jogador um.

--- app/src/main/java/com/example/magiclifecounter/Cena.java
+++ app/src/main/java/com/example/magiclifecounter/Cena.java
@@ -12,6 +12,7 @@ public class Cena extends AGScene {
     public void init() {
         setSceneBackgroundColor(1,1,1);
         criarmarcadores();
+        vidajogardorum();
     }
 
     @Override
@@ -40,4 +41,22 @@ public class Cena extends AGScene {
 
         }
     }
+    public void vidajogardorum(){
+        AGSprite numeroesquerda = null, numerodireita = null;
+
+        numeroesquerda = createSprite(R.mipmap.digit2, 1,1);
+        numeroesquerda.setScreenPercent(20,20);
+        numeroesquerda.vrPosition.setX(250);
+        numeroesquerda.vrPosition.setY(350);
+        numerodireita = createSprite(R.mipmap.digit0, 1,1);
+        numerodireita.setScreenPercent(20,20);
+        numerodireita.vrPosition.setX(400);
+        numerodireita.vrPosition.setY(350);
+
+    }
+
+    public void vidajogadordois(){
+
+    }
+
 }
