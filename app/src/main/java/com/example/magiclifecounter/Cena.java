22492572b5a57c621735443691bc512e9040e237
augusto pugas <augustopugas34@gmail.com>
2021-12-03 14:46:37 -0300

    Adicionando novos métodos e organizando código.

--- app/src/main/java/com/example/magiclifecounter/Cena.java
+++ app/src/main/java/com/example/magiclifecounter/Cena.java
@@ -6,13 +6,16 @@ import com.example.magiclifecounter.AndGraphics.AGSprite;
 public class Cena extends AGScene {
 
     AGSprite[] marcador = new AGSprite[2];
+    AGSprite numeroesquerda, numerodireita = null;
+    int[] referencias = {R.mipmap.digit0,R.mipmap.digit1,R.mipmap.digit2,R.mipmap.digit3,R.mipmap.digit4,R.mipmap.digit5,R.mipmap.digit6,R.mipmap.digit7,R.mipmap.digit8,R.mipmap.digit9};
     float x = (float) 2.9, y = (float) 2.9;
 
     @Override
     public void init() {
         setSceneBackgroundColor(1,1,1);
-        criarmarcadores();
-        vidajogardorum();
+        criarMarcadores();
+        vidaJogardorum();
+        vidaJogadordois();
     }
 
     @Override
@@ -32,7 +35,7 @@ public class Cena extends AGScene {
         }
     }
 
-    public void criarmarcadores(){
+    public void criarMarcadores(){
         int[] referencias = {R.mipmap.squaregreen, R.mipmap.squarered};
         for (int pos = 0 ; pos < marcador.length; pos++){
             marcador[pos] = createSprite(referencias[pos],1,1);
@@ -41,22 +44,50 @@ public class Cena extends AGScene {
 
         }
     }
-    public void vidajogardorum(){
-        AGSprite numeroesquerda = null, numerodireita = null;
+    public void vidaJogardorum(){
+        vidaInicial();
+        posicaoVidaJogadorUm();
+    }
 
-        numeroesquerda = createSprite(R.mipmap.digit2, 1,1);
+    public void vidaJogadordois(){
+        vidaInicial();
+        posicaoVidaJogadordois();
+    }
+
+    public void adicionarVida(){
+
+    }
+
+    public void removerVida(){
+
+    }
+
+    public void resetar(){
+        vidaInicial();
+    }
+
+    public void vidaInicial(){
+        numeroesquerda = createSprite(referencias[2], 1,1);
+        numerodireita = createSprite(referencias[0], 1,1);
+    }
+
+    public void posicaoVidaJogadorUm(){
         numeroesquerda.setScreenPercent(20,20);
         numeroesquerda.vrPosition.setX(250);
         numeroesquerda.vrPosition.setY(350);
-        numerodireita = createSprite(R.mipmap.digit0, 1,1);
         numerodireita.setScreenPercent(20,20);
         numerodireita.vrPosition.setX(400);
         numerodireita.vrPosition.setY(350);
 
     }
 
-    public void vidajogadordois(){
-
+    public void posicaoVidaJogadordois(){
+        numeroesquerda.setScreenPercent(20,20);
+        numeroesquerda.vrPosition.setX(250);
+        numeroesquerda.vrPosition.setY(1150);
+        numerodireita.setScreenPercent(20,20);
+        numerodireita.vrPosition.setX(400);
+        numerodireita.vrPosition.setY(1150);
     }
 
 }
